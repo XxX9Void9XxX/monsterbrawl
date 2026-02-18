608b262010858f63f4e509a3c4cfab44df4c3e25
XxX9Void9XxX <brooksm@carbonschools.org>
2026-02-18 16:10:30 -0700

    Create game.js

---
+++ public/game.js
@@ -0,0 +1,68 @@
+let ws = new WebSocket("wss://" + location.host);
+let currentRoom = null;
+let playerNumber = null;
+
+function signup() {
+  fetch("/signup", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      username: username.value,
+      password: password.value
+    })
+  }).then(r => r.json()).then(d => alert(d.success ? "Signed up!" : "Failed"));
+}
+
+function login() {
+  fetch("/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      username: username.value,
+      password: password.value
+    })
+  }).then(r => r.json()).then(d => {
+    if (d.success) {
+      auth.style.display = "none";
+      game.style.display = "block";
+      loadStats();
+    }
+  });
+}
+
+function loadStats() {
+  fetch("/me")
+    .then(r => r.json())
+    .then(data => {
+      stats.innerText =
+        `Level ${data.level} | XP ${data.xp} | Wins ${data.wins} | Losses ${data.losses}`;
+    });
+}
+
+function findMatch() {
+  ws.send(JSON.stringify({ type: "findMatch" }));
+}
+
+function attack() {
+  ws.send(JSON.stringify({ type: "attack", roomId: currentRoom }));
+}
+
+ws.onmessage = (event) => {
+  const data = JSON.parse(event.data);
+
+  if (data.type === "start") {
+    currentRoom = data.roomId;
+    playerNumber = data.you;
+    battle.style.display = "block";
+  }
+
+  if (data.type === "update") {
+    myhp.innerText = playerNumber === 1 ? data.p1hp : data.p2hp;
+    enemyhp.innerText = playerNumber === 1 ? data.p2hp : data.p1hp;
+  }
+
+  if (data.type === "win") {
+    alert("You won!");
+    battle.style.display = "none";
+  }
+};
